3a3a98877a71030d8a34722f67a6c2d316b52875
Marisa You <marisa.you@gmail.com>
2020-09-04 06:04:05 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,8 +2,20 @@ def generate_num
   return rand(6) + 1
 end
 
-def players_guess
+def get_player_input
   input = gets.chomp
+  return input
+end
 
 def run_guessing_game 
-  generate_num
+  num = generate_num
+  input = get_player_input
+  if input === "exit"
+    puts "Goodbye!"
+  elsif num === input
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{num}."
+  end
+end
+  
